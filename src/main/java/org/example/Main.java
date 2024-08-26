147a6a95a8c8e26f071287ecfa540720dd6ba0e1
Conny <pedalconny@hotmail.com>
2024-08-26 16:02:24 +0200

    Add switch to make choices and made e to ignoreCase

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,39 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Elpriser");
-        System.out.println("========");
-        System.out.println("1. Inmatning");
-        System.out.println("2. Min , Max och Medel");
-        System.out.println("3. Sortera");
-        System.out.println("4. Bästa Laddningstid (4h)");
-        System.out.println("e. Avsluta");
+         String choice;
+
+        do {
+            System.out.println("Elpriser");
+            System.out.println("========");
+            System.out.println("1. Inmatning");
+            System.out.println("2. Min , Max och Medel");
+            System.out.println("3. Sortera");
+            System.out.println("4. Bästa Laddningstid (4h)");
+            System.out.println("e. Avsluta");
+
+            choice = sc.next();
+
+            switch (choice.toLowerCase()) {
+                case "1":
+                    //choice 1
+                    break;
+                case "2":
+                    //choice 2
+                    break;
+                case "3":
+                    //choice 3
+                    break;
+                case "4":
+                    //choice 4
+                    break;
+                case "e":
+                    // choice e
+                    break;
+                default:
+                    System.out.println("Ogiltigt val, försök igen.");
+            }
+
+        }while (choice.equalsIgnoreCase("e"));
     }
 }
